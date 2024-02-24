905d408c2506bbecae3717277ce9c2a1bb922b4a
suriya27-j <132156264+suriya27-j@users.noreply.github.com>
2024-02-24 10:20:30 +0530

    Update About.js

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -6,7 +6,7 @@ function About() {
   const [fetchedEduDetails, setFetchedEduDetails] = useState([eduDetails,personalDetails]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/show_edu_details')
+    fetch('https://roovan-portfolio-2.onrender.com/show_edu_details')
       .then(response => response.json())
       .then(data => {
         setFetchedEduDetails(data);
@@ -20,7 +20,7 @@ function About() {
   const [fetchedworkdetails, setFetchedworkdetails] = useState([eduDetails,personalDetails]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/show_work_details')
+    fetch('https://roovan-portfolio-2.onrender.com/show_work_details')
       .then(response => response.json())
       .then(data => {
         setFetchedworkdetails(data);
@@ -38,7 +38,7 @@ function About() {
       <section>
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
           About Me
-        </h1>
+        </h1>0
         <p className="text-content py-8 lg:max-w-5xl">{fetchedEduDetails[0].About}</p>
       </section>
       
